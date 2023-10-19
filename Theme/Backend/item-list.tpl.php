be9633258d9ee6d15c09481bac6b74a515982729
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-19 21:44:09 +0000

    change icon font to google icons

--- Theme/Backend/item-list.tpl.php
+++ Theme/Backend/item-list.tpl.php
@@ -23,7 +23,7 @@ echo $this->data['nav']->render(); ?>
 <div class="row">
     <div class="col-xs-12">
         <section class="portlet">
-            <div class="portlet-head"><?= $this->getHtml('Items', 'ItemManagement', 'Backend'); ?><i class="lni lni-download download btn end-xs"></i></div>
+            <div class="portlet-head"><?= $this->getHtml('Items', 'ItemManagement', 'Backend'); ?><i class="g-icon download btn end-xs">download</i></div>
             <div class="slider">
             <table id="iSalesItemList" class="default sticky">
                 <thead>
@@ -32,50 +32,50 @@ echo $this->data['nav']->render(); ?>
                     <td><?= $this->getHtml('Number', 'ItemManagement', 'Backend'); ?>
                         <label for="iSalesItemList-sort-1">
                             <input type="radio" name="iSalesItemList-sort" id="iSalesItemList-sort-1">
-                            <i class="sort-asc fa fa-chevron-up"></i>
+                            <i class="sort-asc g-icon">expand_less</i>
                         </label>
                         <label for="iSalesItemList-sort-2">
                             <input type="radio" name="iSalesItemList-sort" id="iSalesItemList-sort-2">
-                            <i class="sort-desc fa fa-chevron-down"></i>
+                            <i class="sort-desc g-icon">expand_more</i>
                         </label>
                         <label>
-                            <i class="filter fa fa-filter"></i>
+                            <i class="filter g-icon">filter_alt</i>
                         </label>
                     <td><?= $this->getHtml('Name', 'ItemManagement', 'Backend'); ?>
                         <label for="iSalesItemList-sort-3">
                             <input type="radio" name="iSalesItemList-sort" id="iSalesItemList-sort-3">
-                            <i class="sort-asc fa fa-chevron-up"></i>
+                            <i class="sort-asc g-icon">expand_less</i>
                         </label>
                         <label for="iSalesItemList-sort-4">
                             <input type="radio" name="iSalesItemList-sort" id="iSalesItemList-sort-4">
-                            <i class="sort-desc fa fa-chevron-down"></i>
+                            <i class="sort-desc g-icon">expand_more</i>
                         </label>
                         <label>
-                            <i class="filter fa fa-filter"></i>
+                            <i class="filter g-icon">filter_alt</i>
                         </label>
                     <td><?= $this->getHtml('Name', 'ItemManagement', 'Backend'); ?>
                         <label for="iSalesItemList-sort-5">
                             <input type="radio" name="iSalesItemList-sort" id="iSalesItemList-sort-5">
-                            <i class="sort-asc fa fa-chevron-up"></i>
+                            <i class="sort-asc g-icon">expand_less</i>
                         </label>
                         <label for="iSalesItemList-sort-6">
                             <input type="radio" name="iSalesItemList-sort" id="iSalesItemList-sort-6">
-                            <i class="sort-desc fa fa-chevron-down"></i>
+                            <i class="sort-desc g-icon">expand_more</i>
                         </label>
                         <label>
-                            <i class="filter fa fa-filter"></i>
+                            <i class="filter g-icon">filter_alt</i>
                         </label>
                     <td class="wf-100"><?= $this->getHtml('Name', 'ItemManagement', 'Backend'); ?>
                         <label for="iSalesItemList-sort-7">
                             <input type="radio" name="iSalesItemList-sort" id="iSalesItemList-sort-7">
-                            <i class="sort-asc fa fa-chevron-up"></i>
+                            <i class="sort-asc g-icon">expand_less</i>
                         </label>
                         <label for="iSalesItemList-sort-8">
                             <input type="radio" name="iSalesItemList-sort" id="iSalesItemList-sort-8">
-                            <i class="sort-desc fa fa-chevron-down"></i>
+                            <i class="sort-desc g-icon">expand_more</i>
                         </label>
                         <label>
-                            <i class="filter fa fa-filter"></i>
+                            <i class="filter g-icon">filter_alt</i>
                         </label>
                 <tbody>
                 <?php $count = 0; foreach ($items as $key => $value) : ++$count;
